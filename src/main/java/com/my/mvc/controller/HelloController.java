a7b382e0b7247876febba598ae8fddf68ea0a5b1
83425 <yushihai2020@163.com>
2021-11-28 22:46:28 +0800

    1

---
+++ src/main/java/com/my/mvc/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.my.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/")
+    public String index(){
+        //返回视图名称
+        return "index";
+    }
+}
